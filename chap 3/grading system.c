57b7ced01f21ba00c1dea8a6ab23d09d63860216
MuhammadFahad-12 <fahaadejaz98@gmail.com>
2025-10-28 20:07:38 +0500

    conditional statements

---
+++ chap 3/grading system.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main ()
+{
+	int marks;
+	printf("Enter marks \n");
+	scanf("%d",&marks);
+	if (marks > 85)
+	{
+		printf("A plus \n");
+			}
+			else if (marks >75){
+				printf("A \n");}			
+					else if (marks > 60){
+					printf("B \n");}
+					else{
+						printf("Fail \n");
+					}
+					
+				
+							}
+	
+
---
+++ chap 3/voting eligibility.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+	int age;
+	printf("Enter age :");
+	scanf("%d",&age);
+	if (age > 18){
+	printf("eligible for vote \n");}
+	else {
+			printf("not eligible for vote \n");
+
+	}
+	
+}

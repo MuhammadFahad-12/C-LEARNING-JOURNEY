8f13f8f4c3e74c150043ece430b5f3ac462afc72
MuhammadFahad-12 <fahaadejaz98@gmail.com>
2025-11-05 23:15:21 +0500

    .

---
+++ chap 3/IF-ELSE  STATEMENTS/divisible_5_11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter the number:\n");
+    scanf("%d", &num);
+
+    if(num % 5 == 0 && num % 11 == 0) {
+        printf("%d is divisible by 5 and 11\n", num);
+    }
+    else {
+        printf("%d is NOT divisible by 5 and 11\n", num);
+    }
+
+    return 0;
+}
---
+++ chap 3/IF-ELSE  STATEMENTS/grading_system.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int marks;
+    printf("Enter the marks:\n");
+    scanf("%d", &marks);
+
+    if(marks >= 90 && marks <= 100) {
+        printf("Grade: A\n");
+    }
+    else if(marks >= 80 && marks < 90) {
+        printf("Grade: B\n");
+    }
+    else if(marks >= 70 && marks < 80) {
+        printf("Grade: C\n");
+    }
+    else if(marks >= 60 && marks < 70) {
+        printf("Grade: D\n");
+    }
+    else {
+        printf("Grade: Fail\n");
+    }
+
+    return 0;
+}

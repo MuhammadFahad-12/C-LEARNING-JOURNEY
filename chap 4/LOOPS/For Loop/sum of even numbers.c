c11de5dc43ba8590ae75e44ff72c88c44ae635dc
MuhammadFahad-12 <fahaadejaz98@gmail.com>
2025-11-07 00:48:25 +0500

    .

--- chap 4/LOOPS/For Loop/sum of even numbers.c
+++ chap 4/LOOPS/For Loop/sum of even numbers.c
@@ -7,12 +7,12 @@ int main() {
     scanf("%d", &n);
 
     for(i = 1; i <= n; i++) {
-        if(i % 2 != 0) {   // Check for ODD
+        if(i % 2 == 0) {   // Check for ODD
             sum = sum + i;
         }
     }
 
-    printf("Sum of odd numbers = %d\n", sum);
+    printf("Sum of even numbers = %d\n", sum);
 
     return 0;
 }

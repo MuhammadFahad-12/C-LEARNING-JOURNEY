dac68e2ecea8f38cd51328942f1a936c44423367
MuhammadFahad-12 <fahaadejaz98@gmail.com>
2025-11-22 15:20:37 +0500

    Create taking input in arrays.c

---
+++ chap 5/ARRAYS/taking input in arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int arr[3];
+	printf("Enter first element :");
+	scanf("%d",&arr[0]);
+	printf("Enter second element :");
+	scanf("%d",&arr[1]);
+	printf("Enter third element :");
+	scanf("%d",&arr[2]);
+	printf("%d",arr[2]);
+	return 0;
+	}
